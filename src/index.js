03e6deca7a12295def50da7476d477c0314a1410
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-03-06 16:08:15 +0800

    Update styles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers/rootReducer";
-import "./index.css";
 import "bulma/css/bulma.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
@@ -25,4 +24,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
